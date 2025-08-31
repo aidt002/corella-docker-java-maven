7a13bb70773d5a99c70f8102d1a13cf73bdc423a
DA admin <227140065+aidt002@users.noreply.github.com>
2025-09-01 01:20:52 +1000

    use progress=plain for streaming docker build output

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -4,7 +4,7 @@ DOCKER_TAG=${1:-my-app}
 DOCKER_DEFAULT_PLATFORM=$2
 
 if [ -n "$DOCKER_DEFAULT_PLATFORM" ]; then
-    docker build --platform "$DOCKER_DEFAULT_PLATFORM" -t "$DOCKER_TAG" .
+    docker build --progress=plain --platform "$DOCKER_DEFAULT_PLATFORM" -t "$DOCKER_TAG" .
 else
-    docker build -t "$DOCKER_TAG" .
+    docker build --progress=plain -t "$DOCKER_TAG" .
 fi
